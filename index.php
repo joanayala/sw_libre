80104b3c0a52e7ade0ac44e00173edb293c0d1fb
Mateo <mateo123daniel@hotmail.com>
2015-05-12 15:16:15 -0500

    Creacion FORMULARIO INDEX.PHP

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-	info();
 	echo "Bienvenidos a nuestra primera app PHP en GitHub";
 ?>
+<html>
+	<head>
+		<title>Formulario Acceso</title>
+	</head>
+	<body>
+		<form action="" name="form2" method="POST">
+	
+	
+			<center>
+			<b>FORMULARIO ACCESO</b><br><br>
+				<b>Usuario</b><br>
+				<input type = "text" name="us"> <br><br>
+				<b>Contraseña</b><br>
+				<input type= "password" name="pass"> <br><br>
+				
+				<input type="submit" action="submit" value="Ingresar">
+			<center>	
+		</form>	
+	<body>
+</html>	
